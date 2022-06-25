35cada8aea5b16b713a3734f956892af79d33109
Raj Pancholi <pancholiraj27@gmail.com>
2022-06-25 20:09:01 +0530

    added window scroll function so then it will infity scroll

--- main.js
+++ main.js
@@ -8,16 +8,19 @@ const displayStatus = document.querySelector("#displayStatus");
 // event listeners
 submitButton.addEventListener("click", infinityScrolling);
 
+// Data
+let pageNumber = 1;
+const apiData = [];
+
 // functions
 async function infinityScrolling(e) {
   e.preventDefault();
+  const userText = textInput.value;
 
   // changing displayStatus
   dataDisplay.innerText = "";
   displayStatus.innerText = "Loading....";
   //   getting text of textInput
-  const userText = textInput.value;
-  const apiData = [];
   await axios
     .get(`https://openlibrary.org/search.json?q=${userText}`)
     // .get(`https://openlibrary.org/search.json?q=marvel`)
@@ -41,3 +44,46 @@ async function infinityScrolling(e) {
     dataDisplay.append(dataH2);
   });
 }
+
+window.addEventListener("scroll", async function () {
+  const userText = textInput.value;
+
+  // check if page has scroll
+  //   console.log(
+  //     "scrollHeight",
+  //     scrollHeight,
+  //     "scrollTop",
+  //     scrollTop,
+  //     "clientHeight",
+  //     clientHeight
+  //   );
+  //   console.log(scrollTop + clientHeight);
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    // console.log((pageNumber = pageNumber + 1));
+
+    await axios
+      .get(
+        `https://openlibrary.org/search.json?q=${userText}?page=${pageNumber++}`
+      )
+      .then(function (response) {
+        response.data.docs.map((data) => {
+          apiData.push(data.title_suggest);
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } else {
+    console.log("can't do generate pages ");
+  }
+
+  // loop over the apiData
+  apiData.map((data) => {
+    // creating h2 for appending to data display
+    const dataH2 = document.createElement("h2");
+    dataH2.innerText = data;
+    // append to display
+    dataDisplay.append(dataH2);
+  });
+});
