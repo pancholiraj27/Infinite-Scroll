38cc448e5fdac01c911f864599a19a2adbe45004
Raj Pancholi <pancholiraj27@gmail.com>
2022-06-25 16:56:32 +0530

    added loding and no data + display result on screen

--- main.js
+++ main.js
@@ -3,27 +3,41 @@ console.log("script loaded...");
 // all selectors
 const textInput = document.querySelector("#textInput");
 const submitButton = document.querySelector("#button");
-
+const dataDisplay = document.querySelector(".displayData");
+const displayStatus = document.querySelector("#displayStatus");
 // event listeners
 submitButton.addEventListener("click", infinityScrolling);
 
 // functions
 async function infinityScrolling(e) {
   e.preventDefault();
+
+  // changing displayStatus
+  dataDisplay.innerText = "";
+  displayStatus.innerText = "Loading....";
   //   getting text of textInput
   const userText = textInput.value;
   const apiData = [];
   await axios
-    // .get(`https://openlibrary.org/search.json?q=${userText}`)
-    .get(`https://openlibrary.org/search.json?q=text`)
+    .get(`https://openlibrary.org/search.json?q=${userText}`)
+    // .get(`https://openlibrary.org/search.json?q=marvel`)
     .then(function (response) {
       response.data.docs.map((data) => {
         apiData.push(data.title_suggest);
       });
-      //   apiData = response.data;
     })
     .catch(function (error) {
       console.log(error);
     });
-  console.log(apiData);
+
+  displayStatus.innerText = "";
+
+  // loop over the apiData
+  apiData.map((data) => {
+    // creating h2 for appending to data display
+    const dataH2 = document.createElement("h2");
+    dataH2.innerText = data;
+    // append to display
+    dataDisplay.append(dataH2);
+  });
 }
